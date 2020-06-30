5e2aee1083ea193731cd4f14e4fb06268d74c0ed
ffayad <ffayad@ptcdb.edu.ps>
2020-06-30 14:12:59 +0300

    google auth and store in DB

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
+const path = require("path");
 const express = require("express");
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const morgan = require(`morgan`);
-const exphbs = require(`express-handlebars`);
-const connectDB = require(`./config/db`);
+const morgan = require("morgan");
+const exphbs = require("express-handlebars");
+const passport = require("passport");
+const session = require("express-session");
+const connectDB = require("./config/db");
 
 //load config
 dotenv.config({ path: "./config/config.env" });
 
+//Passport config
+require("./config/passport")(passport);
 connectDB();
 
 const app = express();
@@ -15,9 +21,35 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan(`dev`));
 }
 
-//handlebars
-app.engine(".hbs", exphbs({ defaultlayout: `main`, extname: ".hbs" }));
+// Handlebars
+app.engine(
+  ".hbs",
+  exphbs({
+    defaultLayout: "main",
+    extname: ".hbs",
+  })
+);
 app.set("view engine", ".hbs");
+
+//Sessions
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+//Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+//Routes
+app.use("/", require("./routes/index"));
+app.use("/auth", require("./routes/auth"));
 
 const PORT = process.env.PORT || 3000;
 
