d3db59697f59732070a48633425e74be5826cf3e
ffayad <ffayad@ptcdb.edu.ps>
2020-07-01 11:28:17 +0300

    add logout, navbar, and store the session in DB

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const morgan = require("morgan");
 const exphbs = require("express-handlebars");
 const passport = require("passport");
 const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
 const connectDB = require("./config/db");
 
 //load config
@@ -37,6 +38,7 @@ app.use(
     secret: "keyboard cat",
     resave: false,
     saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
   })
 );
 
